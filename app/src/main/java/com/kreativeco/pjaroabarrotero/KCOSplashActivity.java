442db9f25e2479ad2f400bfe4e6ed2231324a9d4
JuanCarlosTG <tlaxqueno.guerrero.j.c@gmail.com>
2015-02-24 14:15:32 -0600

    Modifications: parse chrash reporting parse Analitycs Splash Screen

--- app/src/main/java/com/kreativeco/pjaroabarrotero/KCOSplashActivity.java
+++ app/src/main/java/com/kreativeco/pjaroabarrotero/KCOSplashActivity.java
@@ -1,31 +1,46 @@
 package com.kreativeco.pjaroabarrotero;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.parse.Parse;
-import com.parse.ParseAnalytics;
-import com.parse.ParseCrashReporting;
 
-
-public class KCOMainActivity extends Activity {
+public class KCOSplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_kcomain);
-        ParseCrashReporting.enable(this);
-        Parse.initialize(this, String.valueOf(R.string.application_id), String.valueOf(R.string.client_key));
-        ParseAnalytics.trackAppOpenedInBackground(getIntent());
-        throw new RuntimeException("test parse exception");
+        setContentView(R.layout.activity_kcosplash);
+
+        Thread background = new Thread() {
+            public void run() {
+
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(5*1000);
+
+                    // After 5 seconds redirect to another intent
+                    Intent i=new Intent(getBaseContext(), KCOMainActivity.class);
+                    startActivity(i);
+
+                    //Remove activity
+                    finish();
+
+                } catch (Exception e) {
+
+                }
+            }
+        };
+        background.start();
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_kcomain, menu);
+        getMenuInflater().inflate(R.menu.menu_kcosplash, menu);
         return true;
     }
 
@@ -42,5 +57,10 @@ public class KCOMainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    protected void onDestroy()
+    {
+        super.onDestroy();
     }
 }
